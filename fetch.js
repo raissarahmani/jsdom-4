238676ae3e6ec21d7d24bed852381f78c74a7f1c
unknown <raissarahmani18@gmail.com>
2024-12-19 10:40:28 +0700

    edit file

--- fetch.js
+++ fetch.js
@@ -1,23 +1,31 @@
 const url = 'https://jsonplaceholder.typicode.com/users'
 
-fetch(url)
-    .then(response => {
+async function fetchData() {
+    try {
+        const response = await fetch(url)
         if (!response.ok) {
             throw new Error (`Error: ${response.status}`)
         }
-        return response.json()
-    })
-    .then(data => {
-        console.log(data)
 
+        const data = await response.json()
         const outputDiv = document.querySelector("#output")
-        outputDiv.innerHTML = data.map(data => {
-            return `<li>${data.username}</li>`
-        }).join('');
-    })
-    .catch(err => {
-        console.error('There was a problem while fetching data', err)
 
+        data.forEach (user => {
+            const userInfo = document.createElement("div")
+            userInfo.style.border = "1px dashed #000"
+            userInfo.style.borderRadius = "6px"
+            userInfo.style.marginBottom = "1vh"
+            userInfo.style.padding = "1vh 1vw"
+            userInfo.innerHTML = `
+                <p><strong>Name:</strong> ${user.name}</p>
+                <p><strong>Email:</strong> ${user.email}</p>
+                <p><strong>Phone:</strong> ${user.phone}</p>
+                <p><strong>Address:</strong> ${user.address.street}, ${user.address.city}, ${user.address.zipcode}</p>`
+            outputDiv.appendChild(userInfo)
+        })
+    } catch (error) {
         const outputDiv = document.querySelector("#output")
         outputDiv.innerHTML = `<p style="color: red;">Failed to fetch data: ${error.message}</p>`;
-    })
+    }
+}
+fetchData()
